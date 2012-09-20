ea2ea2b20022f796e0f3d854ab39a07fd90a4ce0
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-20 11:52:17 -0300

    Initial Commit

---
+++ GravadorTests/GravadorTests.h
@@ -0,0 +1,13 @@
+//
+//  GravadorTests.h
+//  GravadorTests
+//
+//  Created by Rafael Brigagão Paulino on 20/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GravadorTests : SenTestCase
+
+@end
---
+++ GravadorTests/GravadorTests.m
@@ -0,0 +1,32 @@
+//
+//  GravadorTests.m
+//  GravadorTests
+//
+//  Created by Rafael Brigagão Paulino on 20/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "GravadorTests.h"
+
+@implementation GravadorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GravadorTests");
+}
+
+@end
